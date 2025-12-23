97afe1b50eae5c7268d2dacc7bf6333e9cc44c8b
soyoung <ssssy93@icloud.com>
2025-12-23 14:20:53 +0900

    Js mission

--- JS/fn/fn_mission.js
+++ JS/fn/fn_mission.js
@@ -36,34 +36,69 @@
 // const randomValue2 = random(10,15);
 // console.log('randomValue2 :',randomValue2);
 
-function printStarLine(x) {
-    let star = ''
-    for (let i = 0; i < x; i++){
-        star += '*'
-    }
-    console.log(star)
-}
+// function printStarLine(x) {
+//     let star = ''
+//     for (let i = 0; i < x; i++){
+//         star += '*'
+//     }
+//     console.log(star)
+// }
 
 // printStarLine(5);
 // printStarLine(3);
 
-console.log('-----------------square')
+// console.log('-----------------square')
 
-function printStarSquare(y) {
-    for (let i = 0; i < y; i++) {
-        printStarLine(y)
-    }
+// function printStarSquare(y) {
+//     for (let i = 0; i < y; i++) {
+//         printStarLine(y)
+//     }
+// }
+
+// // printStarSquare(5);
+// // printStarSquare(3);
+
+
+// function printStarTriangle(i) {
+//     for (j = 1; j <= i; j++){
+//         printStarLine(j)
+//     }
+// }
+
+// printStarTriangle(5);
+// printStarTriangle(3);
+
+// const arr = [10, 8, 4, 88, 65, 3];
+// const max = findMax(arr);
+
+// function findMax(arr) {
+//     let max = arr[0];
+//     for (i = 0; i < arr.length; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+// console.log(max)
+
+
+const arr = [10, 8, 4, 88, 65, 3, 99];
+
+// function sumArr(arr) {
+//     let sum = 0;
+//     for (i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+const sumArr = arr => {
+    let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+     }
+    return sum;
 }
 
-// printStarSquare(5);
-// printStarSquare(3);
-
-
-function printStarTriangle(i) {
-    for (j = 1; j <= i; j++){
-        printStarLine(j)
-    }
-}
-
-printStarTriangle(5);
-printStarTriangle(3);
+const sum = sumArr(arr);
+console.log(sum)
